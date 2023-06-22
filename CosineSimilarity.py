781367e3f60a04df2f9535ab1ca1ec715ce453db
Carlos Ardila <carlos.ardila@appgate.com>
2023-06-22 15:11:03 -0500

    Extract Cosine Similarity Method

---
+++ CosineSimilarity.py
@@ -0,0 +1,17 @@
+def cosine_similarity_vectors(vectors1, vectors2):
+    # Combine the strings from both vectors into a single list
+    combined_strings = [' '.join(vectors1), ' '.join(vectors2)]
+
+    # Create a TfidfVectorizer object
+    vectorizer = TfidfVectorizer()
+
+    # Fit and transform the combined strings into TF-IDF vectors
+    vectors = vectorizer.fit_transform(combined_strings)
+
+    # Calculate the cosine similarity matrix
+    similarity_matrix = cosine_similarity(vectors)
+
+    # Extract the cosine similarity score for the pair of interest
+    similarity_score_result = similarity_matrix[0, 1]
+
+    return similarity_score_result
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import SecuenceMatcher
+import CosineSimilarity
 
 # imports for calculate_hash_difference_similarity method
 import hashlib
@@ -11,23 +12,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-def cosine_similarity_vectors(vectors1, vectors2):
-    # Combine the strings from both vectors into a single list
-    combined_strings = [' '.join(vectors1), ' '.join(vectors2)]
 
-    # Create a TfidfVectorizer object
-    vectorizer = TfidfVectorizer()
-
-    # Fit and transform the combined strings into TF-IDF vectors
-    vectors = vectorizer.fit_transform(combined_strings)
-
-    # Calculate the cosine similarity matrix
-    similarity_matrix = cosine_similarity(vectors)
-
-    # Extract the cosine similarity score for the pair of interest
-    similarity_score_result = similarity_matrix[0, 1]
-
-    return similarity_score_result
 
 
 def calculate_identifier(features):
@@ -136,7 +121,7 @@ if __name__ == '__main__':
     print("Time: ", end_time - start_time)
 
     start_time = time.time()
-    similarity_score = cosine_similarity_vectors(strings1, strings2)
+    similarity_score = CosineSimilarity.cosine_similarity_vectors(strings1, strings2)
     end_time = time.time()
 
     print('Cosine Similarity:', similarity_score)
