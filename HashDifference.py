bf0bad6c32751c34e65efa687313121b79da0e95
Carlos Ardila <carlos.ardila@appgate.com>
2023-06-23 14:58:01 -0500

    Small format change

--- HashDifference.py
+++ HashDifference.py
@@ -11,6 +11,7 @@ def calculate_md5hash(features):
     md5_hash = hashlib.md5(combined_string.encode()).hexdigest()
     return md5_hash
 
+
 @MeasureExecTimes.add_logging
 def get_hash_difference_similarity_score(string1, string2):
     pid1 = calculate_md5hash(string1)
